a6981aa60fe0d7b47e6e69f7f36a0ad3264ff9f9
enderphan94 <lvan@rnglabs.net>
2026-06-02 12:13:20 +0700

    markview 2.4.2

--- Casks/markview.rb
+++ Casks/markview.rb
@@ -1,6 +1,6 @@
 cask "markview" do
-  version "2.4.1"
-  sha256 "9807033334dff62a15c97c7462857ab8d103ecd5ff3e46bd2c052fd4e648fb3e"
+  version "2.4.2"
+  sha256 "eec08a690585ca4d8f12ccc21c751b5ca302e1a96398b58fb0862fda20927e14"
 
   url "https://github.com/enderphan94/markdown-app/releases/download/v#{version}/MarkView-#{version}.dmg"
   name "MarkView"
